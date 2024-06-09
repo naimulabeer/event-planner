704604e00815d94482fb5ca70b7934b0bfaef601
naimulabeer <naimul.h.abeer@gmail.com>
2024-06-09 23:20:01 +0600

    document pdf added

--- src/components/Home/HomeGallery.tsx
+++ src/components/Home/HomeGallery.tsx
@@ -1,45 +1,45 @@
-'use client';
+"use client";
 
-import Column from '@/library/utils/column/Column';
-import { Box, Flex, Image } from '@chakra-ui/react';
-import React, { FC } from 'react';
-import { motion, useTransform, useScroll } from 'framer-motion';
-import EventCard from '../Global/EventCard';
+import Column from "@/library/utils/column/Column";
+import { Box, Flex, Image } from "@chakra-ui/react";
+import React, { FC } from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+import EventCard from "../Global/EventCard";
 
 type HomeGalleryProps = {};
 
 const HomeGallery: FC<HomeGalleryProps> = () => {
-	const { scrollY } = useScroll();
-	const y = useTransform(scrollY, [0, 100], [0, 0]);
+  const { scrollY } = useScroll();
+  const y = useTransform(scrollY, [0, 100], [0, 0]);
 
-	return (
-		<Column>
-			<Flex position='relative' p={{ base: 0, lg: 20 }}>
-				<Image
-					w='full'
-					h='fit-content'
-					src='/homegallery.jpg'
-					fill='true'
-					objectFit='cover'
-				/>
+  return (
+    <Column>
+      <Flex position="relative" p={{ base: 0, lg: 20 }}>
+        <Image
+          w="full"
+          h="fit-content"
+          src="/homegallery.jpg"
+          fill="true"
+          objectFit="cover"
+        />
 
-				<Box as={motion.div} style={{ y }}>
-					<EventCard
-						title='GALLERY'
-						description='Unforgettable Memories'
-						buttonText='Discover'
-						bgColor='#AF9453'
-						textColor='#FFFFFF'
-						position='absolute'
-						display={{ base: 'none', xl: 'flex' }}
-						top='50%'
-						left='75%'
-						transform='translate(-50%, -50%)'
-					/>
-				</Box>
-			</Flex>
-		</Column>
-	);
+        <Box as={motion.div} style={{ y }}>
+          <EventCard
+            title="GALLERY"
+            description="Unforgettable Memories"
+            buttonText="Discover"
+            bgColor="#AF9453"
+            textColor="#FFFFFF"
+            position="absolute"
+            display={{ base: "none", xl: "flex" }}
+            top="50%"
+            left="75%"
+            transform="translate(-50%, -50%)"
+          />
+        </Box>
+      </Flex>
+    </Column>
+  );
 };
 
 export default HomeGallery;
